45ecbf733892c6b7abb33e6bc1f1246b0d611896
Zyad Al-Abiad <163039828+Zyadateff@users.noreply.github.com>
2025-12-16 22:44:25 +0200

    Delete Designer Door Mat directory

--- Designer Door Mat/Answer.py
+++
@@ -1,9 +0,0 @@
-n, m = tuple(map(int, input().split()))
-
-for i in range(0, n // 2):
-    print(('.|.'*(2 * i + 1)).center(m, '-'))
-
-print("Love".center(m, '-'))
-
-for i in range(n // 2, 0, -1):
-    print(('.|.'*(2 * i - 1)).center(m, '-'))
